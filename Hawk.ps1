a9d5e8f58159f2be8049cccca522a8ab877e30b4
vrublevskyalex <93339343+vrublevskyalex@users.noreply.github.com>
2022-04-21 16:22:43 -0700

    Add files via upload

---
+++ Hawk.ps1
@@ -0,0 +1,14 @@
+﻿Import-Module ExchangeOnlineManagement
+Connect-ExchangeOnline -UserPrincipalName avrublevsky@linquest.com -ShowProgress $true -ConnectionUri https://outlook.office365.us/powershell-liveid -AzureADAuthorizationEndPointUri https://login.microsoftonline.us/common 
+Connect-IPPSSession -UserPrincipalName avrublevsky@linquest.com  -ConnectionUri https://ps.compliance.protection.office365.us/powershell-liveid/ 
+
+Install-Module Hawk
+Import-Module Hawk
+start-hawktenantinvestigation 
+Start-HawkUserInvestigation -User alex.vrublevsky@linquest.com 
+Get-HawkTenantAzureAuthenticationLogs
+
+MSOnline
+Install-Module MSOnline
+Import-Module MSOnline
+Connect-MsolService -AzureEnvironment USGovernment
